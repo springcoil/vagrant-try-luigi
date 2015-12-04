f9341e41fac6e7c48161acdd080d1c0636e755cc
Vladislav Supalov <vladislav@supalov.com>
2015-12-04 12:58:33 +0100

    Query PostgreSQL in a Luigi task.

--- code/purchases.py
+++ code/purchases.py
@@ -2,6 +2,7 @@ import luigi
 import luigi.postgres
 import csv
 import datetime
+import psycopg2
 
 class ExternalPurchases(luigi.ExternalTask):
     task_namespace = 'testing'
@@ -37,7 +38,7 @@ class PurchasesToDatabase(luigi.postgres.CopyToTable):
     table = "purchases"
 
     columns = [("name", "TEXT"),
-               ("date", "DATE"),
+               ("date", "TIMESTAMP"), #DATE would truncate at the day resolution
                ("price", "INT"),
                ("amount", "INT")]
 
@@ -57,6 +58,44 @@ class PurchasesToDatabase(luigi.postgres.CopyToTable):
 
     def requires(self):
        return ExternalPurchases()
+
+class QueryPostgres(luigi.Task):
+    task_namespace = 'testing'
+
+    def output(self):
+        return luigi.LocalTarget("/home/vagrant/data/same_purchases.csv")
+
+    def requires(self):
+       return PurchasesToDatabase()
+
+    def run(self):
+        # these are here for convenience
+        host = "localhost"
+        database = "vagrantdb"
+        user = "vagrant"
+        password = "vagrant"
+
+        conn = psycopg2.connect(
+            dbname=database,
+            user=user,
+            host=host,
+            password=password)
+        cur = conn.cursor()
+        cur.execute("""SELECT
+          name,
+          date,
+          price,
+          amount
+          FROM purchases
+        """)
+        rows = cur.fetchall()
+
+        with self.output().open("w") as out_file:
+            out_file.write("name, date, price, amount") #write a header by hand
+            for row in rows:
+                out_file.write("\n")
+                # without the :%s, the date will be output in year-month-day format
+                out_file.write("{}, {:%s}, {}, {}".format(*row)) #each element of row by itself, unpacked by the star
 
 if __name__ == '__main__':
     # if you want to execute one task per default without further arguments
